d362e4289f5375af0e35027798ae0e0c3e8e81bd
Arnaud Gourlay <arnaud.gourlay@commercetools.de>
2018-07-13 19:50:49 +0200

    add monitoring and cleanups

--- README.md
+++ README.md
@@ -1,21 +1,70 @@
 # od-crawler
 
-For a given URL it traverses recursively all the links pointing to folders and outputs all the file links found.
+Crawler for open directories.
+
+`Open directories are unprotected directories of pics, vids, music, software and otherwise interesting files.`
+
+## Usage
 
 ```
 ./od-crawler-exe -h
 Usage: od-crawler-exe TARGET [-p|--profile PROFILE] [-v|--verbose]
                       [-d|--directory DIRECTORY]
-  Crawls open directories for tasty links
+  Crawls open directories for interesting links
 
 Available options:
   TARGET                   The target URL or the path to the file containing the
-                           target URLs
+                           target URLs (one per line)
   -p,--profile PROFILE     Profile for allowed extensions (Videos, Pictures,
                            Music, Docs, SubTitles)
-  -v,--verbose             Enable verbose mode
+  -v,--verbose             Enable verbose mode for debugging purpose
   -d,--directory DIRECTORY The folder where to persist results - only new
                            entries will be shown
   --parallel               Crawl target URLS in parallel
+  --monitoring             The monitoring port where metrics are exposed - http://localhost:$port
   -h,--help                Show this help text
 ```
+
+- Examples
+
+```
+./od-crawler-exe http://that.cool.od.site.com
+```
+
+Filter by file extension
+
+```
+./od-crawler-exe http://that.cool.od.site.com -p Videos
+```
+
+The ouput can of course be redirected to a file to enable later search.
+
+```
+./od-crawler-exe http://that.cool.od.site.com -p Videos > results.txt
+```
+
+In order to crawl several URLs at once, a file containing one URL per line can be used as input.
+
+```
+./od-crawler-exe ~/links.txt -p Videos
+```
+
+It is possible to save the result of a run in a folder to only display new items in the next run.
+
+```
+./od-crawler-exe ~/links.txt -d ~/od-crawler/ -p Videos
+```
+
+For more power, let's crawl URLs in parallel!
+
+```
+./od-crawler-exe ~/links.txt -d ~/od-crawler/ -p Videos --parallel
+```
+
+A monitoring interface can be enabled to follow the progress and the internal metrics of the crawler.
+
+```
+./od-crawler-exe ~/links.txt -d ~/od-crawler/ -p Videos --parallel --monitoring 8000
+```
+
+Then open `http://localhost:8000`
--- app/Main.hs
+++ app/Main.hs
@@ -3,15 +3,19 @@
 
 module Main where
 
-import Options.Applicative
+import Options.Applicative -- used as DSL
 import Data.Semigroup ((<>))
+import qualified System.Clock as CS
+import qualified System.Remote.Monitoring as M
+import qualified System.Metrics.Distribution as MD
+import qualified System.Remote.Counter as MC
 import qualified Control.Exception as CE
 import qualified Control.Concurrent.Async as CA
 import qualified System.IO as SI
 import qualified System.Directory as SD
 import qualified Network.HTTP.Simple as NHS
 import qualified Data.HashSet as HS
-import qualified Network.HTTP.Base as NHB
+import qualified Network.URI.Encode as EN
 import qualified Data.ByteString as BS
 import qualified Data.Text.IO as TIO
 import qualified Data.Text as T
@@ -24,17 +28,17 @@ main :: IO ()
 main = execParser opts >>= runWithOptions
   where
     opts = info (optionsParser <**> helper) ( fullDesc
-      <> progDesc "Crawls open directories for tasty links")
+      <> progDesc "Crawls open directories for interesting links")
 
 -- https://github.com/pcapriotti/optparse-applicative
 optionsParser :: Parser Options
 optionsParser =
-  Options <$> targetParser <*> profileParser <*> verbosityParser <*> directoryParser <*> parallelParser
+  Options <$> targetParser <*> profileParser <*> verbosityParser <*> directoryParser <*> parallelParser <*> monitoringParser
 
 targetParser :: Parser String
 targetParser =
   argument str (metavar "TARGET"
-  <> help "The target URL or the path to the file containing the target URLs")
+  <> help "The target URL or the path to the file containing the target URLs (one per line)")
 
 profileParser :: Parser Profile
 profileParser = option auto
@@ -48,7 +52,7 @@ verbosityParser :: Parser Verbosity
 verbosityParser = flag Normal Verbose
   ( long "verbose"
   <> short 'v'
-  <> help "Enable verbose mode" )
+  <> help "Enable verbose mode for debugging purpose" )
 
 directoryParser :: Parser(Maybe String)
 directoryParser = optional (
@@ -62,6 +66,14 @@ parallelParser :: Parser Bool
 parallelParser = switch
   ( long "parallel"
   <> help "Crawl target URLs in parallel" )
+
+monitoringParser :: Parser(Maybe Int)
+monitoringParser = optional (
+  option auto
+   ( long "monitoring"
+    <> short 'm'
+    <> metavar "MONITORING"
+    <> help "The monitoring port where metrics are exposed - http://localhost:$port"))
 
 profileExtensions :: Profile -> AllowedExtensions
 profileExtensions Videos = Only ["mkv", "avi", "mp4"]
@@ -77,13 +89,27 @@ runWithOptions opts = do
   let desiredExtensions = profileExtensions $ profile opts
   let v = verbosity opts
   let df = persistentFolder opts
+  metricsHandler <- handleMonitoring $ monitoring opts
   validatePersistingFolder df
   if parallel opts then
     -- https://hackage.haskell.org/package/async-2.1.0/docs/Control-Concurrent-Async.html
-    CA.mapConcurrently (businessTime desiredExtensions v df) urls
+    -- FIXME limit number of concurrent run
+    CA.mapConcurrently_ (businessTime desiredExtensions v df metricsHandler) urls
   else
-    mapM (businessTime desiredExtensions v df) urls
-  pure ()
+    mapM_ (businessTime desiredExtensions v df metricsHandler) urls
+
+-- https://hackage.haskell.org/package/ekg
+handleMonitoring :: Maybe Int -> IO (Maybe Metrics)
+handleMonitoring Nothing =
+  pure Nothing
+handleMonitoring (Just p) = do
+  handle <- M.forkServer "localhost" p
+  inputUrlsProcessedCounter <- M.getCounter "crawler.input_urls_processed" handle
+  foldersCounter <- M.getCounter "crawler.folders" handle
+  fileCounter <- M.getCounter "crawler.files" handle
+  errorsCounter <- M.getCounter "crawler.errors" handle
+  httpLatencyDistribution <- M.getDistribution "crawler.http_latency_ms" handle
+  pure $ Just $ Metrics httpLatencyDistribution inputUrlsProcessedCounter foldersCounter fileCounter errorsCounter
 
 validatePersistingFolder :: Maybe String -> IO ()
 validatePersistingFolder Nothing = pure ()
@@ -107,19 +133,22 @@ readUrlsFromFile :: String -> IO [String]
 readUrlsFromFile filePath =
   fmap lines (readFile filePath)
 
-businessTime :: AllowedExtensions -> Verbosity -> Maybe String -> String -> IO ()
-businessTime ext v df url =
+businessTime :: AllowedExtensions -> Verbosity -> Maybe String -> Maybe Metrics  -> String -> IO ()
+businessTime ext v df m url =
   case df of
-    Nothing ->
-      crawlUrl (Config ext v Nothing) url
+    Nothing -> do
+      crawlUrl (Config ext v Nothing m) url
+      incrementCounter m inputUrlsProcessed
     Just folderPath -> do
       let fileName = folderPath ++ "/" ++ fileNameForURL url ++ ".txt"
       createFileIfNotExist fileName
       existingContent <- loadPersistedResultsForURL fileName
       SI.withFile fileName SI.AppendMode (\handler ->
         let upc = URLPersistentConfig fileName handler existingContent
-            config = Config ext v (Just upc)
-        in crawlUrl config url)
+            config = Config ext v (Just upc) m
+        in do
+          crawlUrl config url
+          incrementCounter m inputUrlsProcessed)
 
 createFileIfNotExist :: String -> IO ()
 createFileIfNotExist filePath = do
@@ -132,19 +161,21 @@ createFileIfNotExist filePath = do
 
 crawlUrl :: Config -> String -> IO ()
 crawlUrl config url = do
-  safeBody <- safeHttpCall url
+  (safeBody, duration) <- timedMs $ safeHttpCall url
   case safeBody of
     Left ex -> do
       putStrLn ex
+      incrementCounter (metrics config) errors
       pure ()
     Right body -> do
+      addToDistribution (metrics config) httpLatency duration
       let doc = bodyToDoc body
       let extractedLinks = extractLinks doc
       verboseMode config doc extractedLinks url
       let urlTxt = T.pack url
       let links = map (createLink urlTxt) extractedLinks
       let resources = map createResource links
-       --FIXME do not use mapM but an abstraction that allows partial failures
+       -- FIXME handle resources in //
       mapM_ (handleResource config urlTxt) resources
 
 fileNameForURL :: String -> String
@@ -155,9 +186,9 @@ fileNameForURL urlS =
       cleaned = T.replace "/" "-"
   in T.unpack $ (cleaned . afterProtocol . noTrailingSlash) url
 
--- https://hackage.haskell.org/package/hashmap-1.0.0.2/docs/Data-HashSet.html
+-- https://www.stackage.org/haddock/lts-11.17/unordered-containers-0.2.9.0/Data-HashSet.html
 -- A bloom filter should actually be enough to reduce memory footprint
-loadPersistedResultsForURL :: String -> IO (HS.Set T.Text)
+loadPersistedResultsForURL :: String -> IO (HS.HashSet T.Text)
 loadPersistedResultsForURL filePath = do
   fileContent <- TIO.readFile filePath
   let entries = fmap (last . T.splitOn " --> ") (T.lines fileContent)
@@ -182,15 +213,41 @@ handleResource config url r =
           if HS.member (fullLink l) (urlFilecontent cfg)
             then pure ()
             else do
+              --FIXME update Set to avoid possible duplicate in the page
               let pretty = prettyLink l
               TIO.putStrLn pretty
               TIO.hPutStrLn (fileHandle cfg) pretty
+              incrementCounter (metrics config) files
         Nothing ->
           TIO.putStrLn $ prettyLink l
-    Folder l | shouldFollow l url ->
+    Folder l | shouldFollow l url -> do
+        incrementCounter (metrics config) folders
         crawlUrl config (T.unpack $ fullLink l)
     _ ->
         pure ()
+
+timedMs :: IO a -> IO (a, Double)
+timedMs m = do
+    start <- getTimeNs
+    a <- m
+    end <- getTimeNs
+    pure (a,  end / 1000000 - start / 1000000)
+
+-- https://www.stackage.org/haddock/lts-12.0/clock-0.7.2/System-Clock.html
+getTimeNs :: IO Double
+getTimeNs = fmap (fromIntegral . CS.toNanoSecs) (CS.getTime CS.Monotonic)
+
+incrementCounter :: Maybe Metrics -> (Metrics -> MC.Counter) -> IO ()
+incrementCounter mm f =
+  case mm of
+    Nothing -> pure ()
+    Just m ->  MC.inc $ f m
+
+addToDistribution :: Maybe Metrics -> (Metrics -> MD.Distribution) -> Double -> IO ()
+addToDistribution mm f d =
+  case mm of
+    Nothing -> pure ()
+    Just m ->  MD.add (f m) d
 
 linkMatchesConfig :: Config -> Link -> Bool
 linkMatchesConfig config l =
@@ -232,13 +289,11 @@ extractLinks doc =
     &/ element "a"
     &.// attribute "href"
 
+--https://hackage.haskell.org/package/uri-encode-1.5.0.5/docs/Network-URI-Encode.html
 prettyLink :: Link -> T.Text
-prettyLink l =
-  let nameStr = T.unpack (name l)
-      fullNameStr = T.unpack (fullLink l)
-      pretty = NHB.urlDecode nameStr ++ " --> " ++ fullNameStr
-  in T.pack pretty
+prettyLink l = T.concat [EN.decodeText (name l), " --> ", fullLink l]
 
+--FIXME sanitize link e.g '//'
 createLink :: T.Text -> T.Text -> Link
 createLink url display = Link display (T.concat [url, display])
 
@@ -252,11 +307,36 @@ createResource linkResource =
 data Profile = NoProfile | Videos | Music | Pictures | Docs | SubTitles deriving Read
 data Verbosity = Normal | Verbose
 
-data Options = Options { target :: String, profile :: Profile, verbosity :: Verbosity, persistentFolder :: Maybe String, parallel :: Bool}
+data Options = Options {
+  target :: String,
+  profile :: Profile,
+  verbosity :: Verbosity,
+  persistentFolder :: Maybe String,
+  parallel :: Bool,
+  monitoring :: Maybe Int
+}
 
 data AllowedExtensions = AllowAll | Only { allowedExtensions :: [T.Text] }
-data URLPersistentConfig = URLPersistentConfig { urlFilePath :: String, fileHandle :: SI.Handle, urlFilecontent :: HS.Set T.Text}
-data Config = Config { extensions :: AllowedExtensions, debug :: Verbosity, urlPersistentConfig :: Maybe URLPersistentConfig }
+data URLPersistentConfig = URLPersistentConfig {
+  urlFilePath :: String,
+  fileHandle :: SI.Handle,
+  urlFilecontent :: HS.HashSet T.Text
+}
+
+data Metrics =  Metrics {
+  httpLatency :: MD.Distribution,
+  inputUrlsProcessed :: MC.Counter,
+  folders :: MC.Counter,
+  files :: MC.Counter,
+  errors :: MC.Counter
+}
+
+data Config = Config {
+  extensions :: AllowedExtensions,
+  debug :: Verbosity,
+  urlPersistentConfig :: Maybe URLPersistentConfig,
+  metrics :: Maybe Metrics
+}
 
 data Link = Link { name :: T.Text, fullLink :: T.Text } deriving Show
 data Resource = Folder { link :: Link } | File { link :: Link } deriving Show
